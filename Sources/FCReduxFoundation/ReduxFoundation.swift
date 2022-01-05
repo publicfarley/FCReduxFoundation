ec51323538c1e28481e8abdad733f1e5209d0fdb
Farley Caesar <publicfarley@gmail.com>
2022-01-04 21:57:07 -0500

    Added `State` callback and reducer combinator concept.

--- Sources/FCReduxFoundation/ReduxFoundation.swift
+++ Sources/FCReduxFoundation/ReduxFoundation.swift
@@ -8,30 +8,30 @@ import Foundation
 /// `State` is the set of values that represent the overall value of a an application at a point in time.
 /// An  `Action` is a statement of intent to be interpreted by a reducer. Based on the current state, the action results in a new state.
 
-/// A function that takes in the current state and an action, to produce a new state. Since this effectively a "swap" operation on the
-/// immutable `State` value, the Swift `inout` keyword is leveraged so that the compiler handles wrting the boilerplate code
-/// to replace the old state with the new state.
-public typealias Reducer<State: Equatable, Action> = (inout State, Action) -> Void
+/// A function that takes in the current state and an action, to produce a new state. This is effectively a "swap" operation on the
+/// immutable `State` value. The reducer communicates the new state by calling the callback `(State) -> Void` function.
+public typealias Reducer<State, Action> = (Action, State, (State) -> Void) -> Void
 
 /// A function given to middleware by the store, to give it a location to dispatch actions
 public typealias Dispatcher<Action> = (Action) -> Void
 
 /// A function that takes a state, an action, and a dispatcher function.
-/// Middleware is where inpure functions of state and action are processed. That is, middleware functions can produce side effects based on the given action and state. They can also dispatch a resulting action using the dipatcher.
-public typealias Middleware<State: Equatable, Action> = (State, Action, @escaping Dispatcher<Action>) -> Void
+/// Middleware is where inpure functions of state and action are processed. That is, middleware functions can produce side
+/// effects based on the given action and state. They can also dispatch a resulting action using the dipatcher.
+public typealias Middleware<State, Action> = (State, Action, @escaping Dispatcher<Action>) -> Void
 
 /// A `Store` is place that holds the current state, the store reducer, and store middleware. It provides a facility where external actors can dispatch actions to the store.
 ///  - The store runs the reducer when actions arive to produce a new state for the store to hold on to and publish.
 ///  - The store runs all middleware when actions arrive as well.
-@MainActor public final class Store<State: Equatable, Action>: ObservableObject {
+@MainActor public final class Store<State, Action>: ObservableObject {
     @Published public private(set) var state: State
     
-    private let reducer: Reducer<State: Equatable, Action>
-    private let middleware: [Middleware<State: Equatable, Action>]
+    private let reducer: Reducer<State, Action>
+    private let middleware: [Middleware<State, Action>]
     
     public init(initialState: State,
-         reducer: @escaping Reducer<State: Equatable, Action>,
-         middleware: [Middleware<State: Equatable, Action>]) {
+         reducer: @escaping Reducer<State, Action>,
+         middleware: [Middleware<State, Action>]) {
         
         self.state = initialState
         self.reducer = reducer
@@ -39,7 +39,9 @@ public typealias Middleware<State: Equatable, Action> = (State, Action, @escapin
     }
 
     public func dispatch(_ action: Action) {
-        reducer(&state,action)
+        reducer(action, state) { state in
+            self.state = state
+        }
                 
         middleware.forEach { middlewareInstance in
             middlewareInstance(state, action) { action in
@@ -52,27 +54,28 @@ public typealias Middleware<State: Equatable, Action> = (State, Action, @escapin
 }
 
 /// Combine takes two reducers and returns a single reducer function. This function first calls the first reducer
-/// If it effected the state, then it just returns that state, otherwise it returns the result of the 2nd reducer.
-/// This way only one reducer can affect the state. So each Action can only be acted on by a single reducer.
-func combine<State: Equatable, Action>(_ reducer1: @escaping Reducer<State, Action>, 
-                            with reducer2: @escaping Reducer<State, Action>) -> Reducer<State, Action> {
-    { (state: inout State, action: Action) -> Void in
-        let oldState = state
-        reducer1(&state,action)
-        let newState = state
+/// If it effected the state, then it "returns" the new state by calling the callback and returns early. Otherwise calls the 2nd reducer.
+/// This way, only one reducer can affect the state. So each Action can only be acted on by a single reducer.
+func combine<State, Action>(_ lhsReducer: @escaping Reducer<State, Action>,
+                            with rhsReducer: @escaping Reducer<State, Action>) -> Reducer<State, Action> {
+    
+    { (action: Action, state: State, stateUpdater: (State) -> Void) -> Void in
+        var isUpdaterCalled = false
         
-        let isStateUnchanged = oldState == newState
-        guard isStateUnchanged else {
-            // first reducer took care of modifying the state so make an early exit
-            return
+        lhsReducer(action, state) { state in
+            isUpdaterCalled = true
+            stateUpdater(state)
         }
         
-        reducer2(&state,action)
+        guard !isUpdaterCalled else { return }
+        
+        rhsReducer(action, state, stateUpdater)
     }
 }
 
-public func reduce<State: Equatable, Action>(reducers: [Reducer<State, Action>]) -> Reducer<State: Equatable, Action> {
-    let noOpReducer: Reducer<State: Equatable, Action> = { _, _ in }
+/// Combines a list of reducers into one reducer.
+func reduce<State, Action>(reducers: [Reducer<State, Action>]) -> Reducer<State, Action> {
+    let noOpReducer: Reducer<State, Action> = { _, _, _ in }
     
     let singleReducer = reducers.reduce(noOpReducer) { currentReducer, nextReducer in
         return combine(currentReducer, with: nextReducer)
@@ -80,3 +83,5 @@ public func reduce<State: Equatable, Action>(reducers: [Reducer<State, Action>])
     
     return singleReducer
 }
+
+
--- Tests/FCReduxFoundationTests/FCReduxFoundationTests.swift
+++ Tests/FCReduxFoundationTests/FCReduxFoundationTests.swift
@@ -2,6 +2,77 @@ import XCTest
 @testable import FCReduxFoundation
 
 final class FCReduxFoundationTests: XCTestCase {
+    
+    #warning("Write proper test cases")
     func testExample() throws {
+        enum AppAction {
+            case one, two, three
+        }
+
+        struct AppState: Equatable {
+            var value: String
+        }
+        
+        func reducer_1(action: AppAction, state: AppState, communicator: (AppState) -> Void) {
+            switch action {
+            case .one:
+                var newState = state
+                newState.value = "One"
+                communicator(newState)
+            default:
+                return
+            }
+        }
+
+        func reducer_2(action: AppAction, state: AppState, communicator: (AppState) -> Void) {
+            switch action {
+            case .two:
+                var newState = state
+                newState.value = "Two"
+                communicator(newState)
+            default:
+                return
+            }
+        }
+
+        func reducer_3(action: AppAction, state: AppState, communicator: (AppState) -> Void) {
+            if case AppAction.three = action {
+                var newState = state
+                newState.value = "Three"
+                communicator(newState)
+            }
+        }
+
+
+        class AppStore_1 {
+            private (set) var state: AppState
+            let reducer: Reducer<AppState, AppAction>
+            
+            init(state: AppState, reducer: @escaping Reducer<AppState, AppAction>) {
+                self.state = state
+                self.reducer = reducer
+            }
+            
+            func dispatch(_ action: AppAction) {
+                reducer(action, state) { state in
+                    self.state = state
+                }
+            }
+        }
+
+        let masterReducer_1 = reduce(reducers: [reducer_1, reducer_2, reducer_3])
+
+        let store_1 = AppStore_1(state: AppState(value: "initial"), reducer: masterReducer_1)
+
+        print("\n------------------------\n")
+
+        print(store_1.state.value)
+
+        store_1.dispatch(.one)
+        print(store_1.state.value)
+
+        store_1.dispatch(.three)
+        print(store_1.state.value)
+
     }
 }
