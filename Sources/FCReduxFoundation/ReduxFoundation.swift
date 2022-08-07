b87875ce43fba6ded6d8a211eadb3a42249bed82
Farley Caesar <publicfarley@gmail.com>
2022-08-07 15:50:38 -0400

    Updated the Redux engine to facilitate a distributed reducer approach.
    Thus removing the need for a large switch statement to reduce the current state to the new state.
    This better enables team development and aligns the code with the SOLID Open/Closed principle.

--- Sources/FCReduxFoundation/ReduxFoundation.swift
+++ Sources/FCReduxFoundation/ReduxFoundation.swift
@@ -5,51 +5,80 @@
 //
 import Foundation
 
-/// `State` is the set of values that represent the overall value of a an application at a point in time.
-/// An  `Action` is a statement of intent to be interpreted by a reducer. Based on the current state, the action results in a new state.
-
-/// A function that takes in the current state and an action, to produce a new state. Since this effectively a "swap" operation on the
-/// immutable `State` value, the Swift `inout` keyword is leveraged so that the compiler handles wrting the boilerplate code
-/// to replace the old state with the new state.
-public typealias Reducer<State, Action> = (inout State, Action) -> Void
-
-/// A function given to middleware by the store, to give it a location to dispatch actions
-public typealias Dispatcher<Action> = (Action) -> Void
-
-/// A function that takes a state, an action, and a dispatcher function.
-/// Middleware is where inpure functions of state and action are processed. That is, middleware functions can produce side effects based on the given action and state. They can also dispatch a resulting action using the dipatcher.
-public typealias Middleware<State, Action> = (State, Action, @escaping Dispatcher<Action>) -> Void
+typealias Action<State> = Store<State>.Action
 
 /// A `Store` is place that holds the current state, the store reducer, and store middleware. It provides a facility where external actors can dispatch actions to the store.
 ///  - The store runs the reducer when actions arive to produce a new state for the store to hold on to and publish.
 ///  - The store runs all middleware when actions arrive as well.
-@MainActor public final class Store<State,Action>: ObservableObject {
-    @Published public private(set) var state: State
-    
-    private let reducer: Reducer<State, Action>
-    private let middleware: [Middleware<State, Action>]
-    
-    public init(initialState: State,
-         reducer: @escaping Reducer<State, Action>,
-         middleware: [Middleware<State, Action>]) {
-        
-        self.state = initialState
-        self.reducer = reducer
-        self.middleware = middleware
+/// - Parameters:
+///     - State: The set of values that represent the overall value of a an application at a point in time.
+
+// Note: Usage of MainActor as per advice below: https://www.hackingwithswift.com/quick-start/concurrency/how-to-use-mainactor-to-run-code-on-the-main-queue
+@MainActor
+final class Store<State>: ObservableObject {
+    enum ProcessDirective {
+        case `continue`
+        case terminate
     }
 
-    public func dispatch(_ action: Action) {
-        reducer(&state,action)
-                
-        middleware.forEach { middlewareInstance in
-            middlewareInstance(state, action) { action in
-                DispatchQueue.main.async { [weak self] in
-                    self?.dispatch(action)
-                }
+    /// A function given to middleware by the store, to give it a location to dispatch actions
+    typealias Dispatcher = @MainActor (Action) -> Void
+    
+    /// Middleware is where inpure functions of state and action are processed.
+    /// That is, middleware functions can produce side effects based on the given action and state.
+    /// They can also dispatch a resulting action using the dipatcher.
+    /// Finally they return a `ProcessDirective` which tells the store whether to continue to process the action or to termiate processing.
+    /// A directive to terminate means the action will not proceed to the reducer.
+    ///
+    /// GeneralMiddleware: A middleware function that takes a state, an action, and a dispatcher function.
+    typealias GeneralMiddleware = (State, Action, @escaping Dispatcher) -> ProcessDirective
+
+    /// An  `Action` is a statement of intent to be interpreted by a reducer. Based on the current state, the action results in a new state.
+    struct Action {
+        /// SpecificMiddleware: A middleware function tied to a specific action that takes a state and a dispatcher function.
+        typealias SpecificMiddleware = (State, @escaping Dispatcher) -> ProcessDirective
+
+        let name: String
+        let reduce: (inout State) -> Void
+        let middleware: SpecificMiddleware?
+        
+        init(name: String,
+             reduce: @escaping (inout State) -> Void,
+             middleware: SpecificMiddleware? = nil
+        ) {
+            self.name = name
+            self.reduce = reduce
+            self.middleware = middleware
+        }
+    }
+    
+    @Published private (set) var state: State
+    var middleware: [GeneralMiddleware]
+    
+    init(state: State, middleware: [GeneralMiddleware] = []) {
+        self.state = state
+        self.middleware = middleware
+    }
+    
+    func dispatch(_ action: Action) {
+        
+        // General Middleware
+        for middlewareInstance in middleware {
+            if middlewareInstance(state, action, dispatch) == .terminate {
+                return
             }
         }
-    }    
+        
+        guard let specificMiddleware = action.middleware else {
+            // No specific middleware, just reduce
+            action.reduce(&state)
+            return
+        }
+        
+        // Run Specific Middleware, then reduce if allowed
+        if specificMiddleware(state, dispatch) == .continue {
+            action.reduce(&state)
+        }
+    }
 }
-
-
 
