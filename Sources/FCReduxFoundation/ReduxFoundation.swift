78a37fcd6f7390fbc6ed98ec36dfaf7218037a2d
Farley Caesar <publicfarley@gmail.com>
2022-09-22 22:19:56 -0400

    - Added "out of the box logging" - Added a new protocol `ParameterizedAction` and implementing type: `StandardParameterizedAction` to provide library support for actions that take arguments - Added some simple unit tests

--- Sources/FCReduxFoundation/ReduxFoundation.swift
+++ Sources/FCReduxFoundation/ReduxFoundation.swift
@@ -10,22 +10,42 @@ public enum ProcessDirective {
     case terminate
 }
 
-/// An  `Action` is a statement of intent to be interpreted by a reducer. Based on the current state, the action results in a new state.
-public protocol Action {
+/// An  `Action` is a statement of intent to be interpreted by a reducer.
+/// Based on the current state, processing the action results in a new state.
+public protocol MiddlewareProvider {
     associatedtype State
     associatedtype Environment
-    
-    typealias Reducer = (inout State) -> Void
 
     /// Middleware: A fpotentially side effecting function tied to a specific action that takes a Store.
     typealias Middleware = (Store<Environment, State>) -> ProcessDirective
-    
-    var name: String { get }
-    var reduce: Reducer { get }
+
     var middleware: Middleware { get }
 }
 
+public protocol Action: MiddlewareProvider, Identifiable {
+    
+    typealias Reducer = (inout State) -> Void
+    
+    var reduce: Reducer { get }
+}
+
 public extension Action {
+    var middleware: Middleware {
+        { store in
+            .continue
+        }
+    }
+}
+
+public protocol ParameterizedAction: MiddlewareProvider, Identifiable {
+    associatedtype Parameters
+    typealias Reducer = (inout State, Parameters) -> Void
+    
+    var argument: Parameters { get }
+    var reduce: Reducer { get }
+}
+
+public extension ParameterizedAction {
     var middleware: Middleware {
         { store in
             .continue
@@ -47,45 +67,113 @@ public final class Store<Environment, State>: ObservableObject {
     /// A directive to terminate means the action will not proceed to the reducer.
     ///
     /// GeneralMiddleware: A middleware function that takes an action, and the store. Returns a `ProcessDirective`
-    public typealias GeneralMiddleware = @MainActor (any Action, Store<Environment, State>) -> ProcessDirective
-
+    public typealias GeneralMiddleware = @MainActor (any Identifiable, Store<Environment, State>) -> ProcessDirective
+    
+    public typealias Logger = (String) -> Void
+    
     @Published public private (set) var state: State
     
     public let middleware: [GeneralMiddleware]
     public let environment: Environment
+    public let logger: Logger
     
-    public init(environment: Environment, state: State, middleware: [GeneralMiddleware] = []) {
+    public init(
+        environment: Environment,
+        state: State,
+        middleware: [GeneralMiddleware] = [],
+        logger: @escaping Logger = { _ in }
+    ) {
         self.state = state
         self.middleware = middleware
         self.environment = environment
+        self.logger = logger
     }
     
     public func dispatch<T: Action>(_ action: T) where T.State == State, T.Environment == Environment {
+        
+        reduce(action: action) { state in
+            // Run Specific Middleware, then reduce if allowed
+            if action.middleware(self) == .continue {
+                action.reduce(&state)
+            }
+        }
+    }
 
-        // General Middleware
+    public func dispatch<T: ParameterizedAction>(_ action: T) where T.State == State, T.Environment == Environment {
+        
+        reduce(action: action) { state in
+            // Run Specific Middleware, then reduce if allowed
+            if action.middleware(self) == .continue {
+                action.reduce(&state, action.argument)
+            }
+        }
+    }
+    
+    private func reduce(
+        action: any Identifiable,
+        using reduce: (inout State) -> Void
+    ) {
+        logger("****\nState *before* action: '\(action)':\n\(state)\n****\n")
+        
+        // Run General Middleware
         for middlewareInstance in middleware {
             if middlewareInstance(action, self) == .terminate {
                 return
             }
         }
         
-        // Run Specific Middleware, then reduce if allowed
-        if action.middleware(self) == .continue {
-            action.reduce(&state)
-        }
+        // Run the reduction
+        reduce(&state)
+        
+        logger("****\nState *after* action: '\(action)':\n\(state)\n****\n")
     }
 }
 
 public struct StandardAction<Environment, State>: Action {
     public typealias State = State
     
-    public let name: String
+    public let id: String
     public let reduce: Reducer
     public let middleware: Middleware
     
-    public init(name: String, reduce: @escaping Reducer, middleware: Middleware? = nil) {
-        self.name = name
+    public init(id: String, reduce: @escaping Reducer, middleware: Middleware? = nil) {
+        self.id = id
         self.reduce = reduce
         self.middleware = middleware ?? { _ in .continue }
+    }
+}
+
+extension StandardAction: CustomStringConvertible {
+    public var description: String {
+        id
+    }
+}
+
+public struct StandardParameterizedAction<Environment, State, Parameters>: ParameterizedAction {
+    public typealias State = State
+    public typealias Parameters = Parameters
+    
+    public let id: String
+    public let reduce: ParameterizedReducer
+    public let middleware: Middleware
+    public let argument: Parameters
+    
+    public typealias ParameterizedReducer = (inout State, Parameters) -> Void
+
+    public init(id: String,
+                argument: Parameters,
+                reduce: @escaping ParameterizedReducer,
+                middleware: Middleware? = nil
+    ) {
+        self.id = id
+        self.argument = argument
+        self.reduce = reduce
+        self.middleware = middleware ?? { _ in .continue }
+    }
+}
+
+extension StandardParameterizedAction: CustomStringConvertible {
+    public var description: String {
+        "\(id)(\(argument))"
     }
 }
--- Tests/FCReduxFoundationTests/FCReduxFoundationTests.swift
+++ Tests/FCReduxFoundationTests/FCReduxFoundationTests.swift
@@ -1,7 +1,55 @@
+import OSLog
 import XCTest
 @testable import FCReduxFoundation
 
-final class FCReduxFoundationTests: XCTestCase {
-    func testExample() throws {
+final class ActionTests: XCTestCase {
+    var store: Store<Environment, Counter>!
+    
+    @MainActor override func setUp() {
+        super.setUp()
+        store = Store(environment: Environment(), state: Counter(count: 0), logger: { print($0) })
+        
     }
+    
+    @MainActor override func tearDown() {
+        store = nil
+        super.tearDown()
+    }
+    
+    @MainActor func testIncrement() throws {
+        let increment = StandardAction<Environment, Counter>(id: "increment") { counter in
+            counter.count += 1
+        }
+
+        store.dispatch(increment)
+        
+        XCTAssertEqual(store.state.count, 1)
+    }
+
+    @MainActor func testDecrement() throws {
+        let decrement = StandardAction<Environment, Counter>(id: "decrement") { counter in
+            counter.count -= 1
+        }
+
+        store.dispatch(decrement)
+        
+        XCTAssertEqual(store.state.count, -1)
+    }
+
+    @MainActor func testSet() throws {
+        let specificValue = 42
+        let set = StandardParameterizedAction<Environment, Counter, Int>(id: "set", argument: specificValue) { counter, value in
+            counter.count = value
+        }
+
+        store.dispatch(set)
+        
+        XCTAssertEqual(specificValue, store.state.count)
+    }
+}
+
+struct Environment {}
+
+struct Counter {
+    var count: Int
 }
